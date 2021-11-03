5c5ace5dbd89b7e95a1606052be96ef4334d64b2
FridaRodriguezPa <89856440+FridaRodriguezPa@users.noreply.github.com>
2021-11-03 00:16:43 +0000

    Deploying to gh-pages from @ NiverMtz/projecto-integrador-generation@9a6ee438ffa725717d174e8f8983c02fab381d6d 🚀

--- assets/sql/base-datos.sql
+++ assets/sql/base-datos.sql
@@ -6,7 +6,7 @@ USE paax;
 
 -- Crear tabla de usuarios
 CREATE TABLE usuarios(
-    id_usuarios     INT AUTO_INCREMENT NOT NULL ,
+    id_usuarios     INT  NOT NULL ,
     nombres         NVARCHAR(50) NOT NULL,
     apellido_p      NVARCHAR(50) NOT NULL,
     apellido_m      NVARCHAR(50) NOT NULL,
@@ -14,18 +14,17 @@ CREATE TABLE usuarios(
     contrasenia     NVARCHAR(50) NOT NULL,
     edad_min        BOOLEAN NOT NULL,
     nombre_usuario  NVARCHAR(50) NOT NULL,
-    CONSTRAINT PK_id_usuarios PRIMARY KEY (id)
+    CONSTRAINT PK_id_usuarios PRIMARY KEY (id_usuarios)
 );
 
 -- Crear tabla usuario-post
 CREATE TABLE usuarios_post(
-    id_usuarios_post INT AUTO_INCREMENT NOT NULL
-    id_usuarios INT NOT NULL,
+    id_usuarios_post INT AUTO_INCREMENT NOT NULL,
+    id_usuarios  INT NOT NULL,
     id_post INT NOT NULL,
-    CONSTRAINT PK_id_usuarios_post PRIMARY KEY (id),
-    FOREIGN KEY  id_usuarios REFERENCES id_usuarios(id_usuarios),
-    FOREIGN KEY  id_post REFERENCES id_post(id_post)
-    
+    CONSTRAINT PK_id_usuarios_post PRIMARY KEY (id_usuarios_post),
+    FOREIGN KEY  (id_usuarios) REFERENCES usuarios(id_usuarios),
+    FOREIGN KEY  (id_post) REFERENCES post(id_post)
 );
 
 -- Crear tabla post
@@ -34,17 +33,17 @@ CREATE TABLE post(
     titulo_pista NVARCHAR(100) NOT NULL,
     titulo_album NVARCHAR(100) NOT NULL,
     img_album    VARCHAR(100) NOT NULL,
-    CONSTRAINT PK_id_post PRIMARY KEY (id)
+    CONSTRAINT PK_id_post PRIMARY KEY (id_post)
 );
 
---Crear tabla post - reseña
+-- Crear tabla post - reseña
 CREATE TABLE post_resenia(
     id_post_resenia INT AUTO_INCREMENT NOT NULL,
     id_post INT NOT NULL,
     id_resenia INT NOT NULL,
-    CONSTRAINT PK_id_post_resenia PRIMARY KEY (id),
-    FOREIGN KEY  id_post REFERENCES id_post(id_post),
-    FOREIGN KEY  id_resenia REFERENCES id_resenia(id_resenia)
+    CONSTRAINT PK_id_post_resenia PRIMARY KEY (id_post_resenia),
+    FOREIGN KEY  (id_post) REFERENCES post(id_post),
+    FOREIGN KEY  (id_resenia) REFERENCES resenia(id_resenia)
 );
 
 -- Crear tabla reseña
@@ -53,8 +52,8 @@ CREATE TABLE resenia(
     fecha_publicacion DATE NOT NULL,
     titulo_resenia  NVARCHAR(100) NOT NULL,
     id_usuarios INT NOT NULL,
-    CONSTRAINT PK_id_resenia PRIMARY KEY (id),
-    FOREIGN KEY  id_usuarios REFERENCES id_usuarios(id_usuarios)
+    CONSTRAINT PK_id_resenia PRIMARY KEY (id_resenia),
+    FOREIGN KEY  (id_usuarios) REFERENCES usuarios(id_usuarios)
 );
 
 -- Crear tabla resenia - comentario
@@ -62,14 +61,14 @@ CREATE TABLE resenia_comentario(
     id_resenia_comentario INT AUTO_INCREMENT NOT NULL,
     id_resenia INT NOT NULL,
     id_comentario INT NOT NULL,
-    CONSTRAINT PK_id_resenia_comentario PRIMARY KEY (id),
-    FOREIGN KEY  id_resenia REFERENCES id_resenia(id_resenia),
-    FOREIGN KEY  id_comentario REFERENCES id_comentario(id_comentario)
+    CONSTRAINT PK_id_resenia_comentario PRIMARY KEY (id_resenia_comentario),
+    FOREIGN KEY  (id_resenia) REFERENCES resenia(id_resenia),
+    FOREIGN KEY  (id_comentario) REFERENCES comentario(id_comentario)
 );
 
---Crear tabla comentario
+-- Crear tabla comentario
 CREATE TABLE comentario(
     id_comentario INT AUTO_INCREMENT NOT NULL,
     comentario NVARCHAR(255) NOT NULL,
-    CONSTRAINT PK_id_comentario PRIMARY KEY (id)
+    CONSTRAINT PK_id_comentario PRIMARY KEY (id_comentario)
 );

5815dd66ffc2d0b5665f9ac4d88297e5aef23573
Frida Rodriguez <fridaixchelrp@gmail.com>
2021-11-05 12:27:17 -0600

    Cambios

---
+++ src/assets/sql/gsdfgdfsg.sql
@@ -0,0 +1,9 @@
+	INSERT INTO `db_example`.`Users` (`id`, `name`, `last_name`, `email`) VALUES ('1', 'Debbie', 'Ly', 'debbie@generation.org');
+    INSERT INTO `db_example`.`Users` (`id`, `name`, `last_name`, `email`) VALUES ('2', 'Cleon', 'Grant', 'cleon@generation.org');
+    INSERT INTO `db_example`.`Users` (`id`, `name`, `last_name`, `email`) VALUES ('3', 'Carlos', 'Echeverry', 'carlos@generation.org');
+    INSERT INTO `db_example`.`Users` (`id`, `name`, `last_name`, `email`) VALUES ('4', 'Elise', 'Guimarães', 'elise@generation.org');
+    INSERT INTO `db_example`.`Users` (`id`, `name`, `last_name`, `email`) VALUES ('5', 'Ian', 'Munene', 'ian@generation.org');
+    INSERT INTO `db_example`.`Users` (`id`, `name`, `last_name`, `email`) VALUES ('6', 'Santiago', 'Carrillo', 'santiago@generation.org');
+    
+    use db_example;
+    

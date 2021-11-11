6bed8baf44ac4eb646885adde5c18dda8bb38c39
VHIzq <85770256+VHIzq@users.noreply.github.com>
2021-11-11 03:59:11 +0000

    Deploying to gh-pages from @ NiverMtz/projecto-integrador-generation@f668f11f655fdb9adc9ab18283003cac649f34f3 🚀

--- assets/sql/base-datos.sql
+++ assets/sql/base-datos.sql
@@ -17,16 +17,6 @@ CREATE TABLE usuarios(
     CONSTRAINT PK_id_usuarios PRIMARY KEY (id_usuarios)
 );
 
--- Crear tabla usuario-post
-CREATE TABLE usuarios_post(
-    id_usuarios_post INT AUTO_INCREMENT NOT NULL,
-    id_usuarios  INT NOT NULL,
-    id_post INT NOT NULL,
-    CONSTRAINT PK_id_usuarios_post PRIMARY KEY (id_usuarios_post),
-    FOREIGN KEY  (id_usuarios) REFERENCES usuarios(id_usuarios),
-    FOREIGN KEY  (id_post) REFERENCES post(id_post)
-);
-
 -- Crear tabla post
 CREATE TABLE post(
     id_post INT AUTO_INCREMENT NOT NULL,
@@ -34,16 +24,6 @@ CREATE TABLE post(
     titulo_album NVARCHAR(100) NOT NULL,
     img_album    VARCHAR(100) NOT NULL,
     CONSTRAINT PK_id_post PRIMARY KEY (id_post)
-);
-
--- Crear tabla post - reseña
-CREATE TABLE post_resenia(
-    id_post_resenia INT AUTO_INCREMENT NOT NULL,
-    id_post INT NOT NULL,
-    id_resenia INT NOT NULL,
-    CONSTRAINT PK_id_post_resenia PRIMARY KEY (id_post_resenia),
-    FOREIGN KEY  (id_post) REFERENCES post(id_post),
-    FOREIGN KEY  (id_resenia) REFERENCES resenia(id_resenia)
 );
 
 -- Crear tabla reseña
@@ -54,16 +34,6 @@ CREATE TABLE resenia(
     id_usuarios INT NOT NULL,
     CONSTRAINT PK_id_resenia PRIMARY KEY (id_resenia),
     FOREIGN KEY  (id_usuarios) REFERENCES usuarios(id_usuarios)
-);
-
--- Crear tabla resenia - comentario
-CREATE TABLE resenia_comentario(
-    id_resenia_comentario INT AUTO_INCREMENT NOT NULL,
-    id_resenia INT NOT NULL,
-    id_comentario INT NOT NULL,
-    CONSTRAINT PK_id_resenia_comentario PRIMARY KEY (id_resenia_comentario),
-    FOREIGN KEY  (id_resenia) REFERENCES resenia(id_resenia),
-    FOREIGN KEY  (id_comentario) REFERENCES comentario(id_comentario)
 );
 
 -- Crear tabla comentario
